6d15f1d93ba086ca52ec6fd2f7709cf428b853ca
Bruno Peselli <brunopeselli@gmail.com>
2023-03-21 20:10:52 -0300

    Update

---
+++ functions/getUserParticipationOnRole.js
@@ -0,0 +1,26 @@
+const { ObjectId } = require("mongodb");
+
+async function getUserParticipationOnRole(userId, roleId, rolesCollection) {
+  
+  try {
+    let confirmed = false;
+    await rolesCollection
+      .findOne({
+        _id: new ObjectId(roleId),
+        participantsRole: { $in: [userId] },
+      })
+      .then((result) => {
+        if (result) {
+          confirmed = true;
+        } else {
+          confirmed = false;
+        }
+      });
+    return confirmed;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+module.exports = getUserParticipationOnRole;
